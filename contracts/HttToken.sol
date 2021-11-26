c7a1b45c68ccc4789b14bb22db130c1154b03bc5
Riviere Fougy <rivierefougy@gmail.com>
2021-11-26 17:55:19 -0500

    installed @openzeppelin/contracts, HTTToken.sol: created token name and initialSupply, 2_deply_contracts.js: added total amount

--- contracts/HttToken.sol
+++ contracts/HttToken.sol
@@ -1,5 +1,9 @@
 pragma solidity ^0.8.0;
 
-contract HttToken {
-  
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract HTTToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("Hodl the Throdl Token", "HTT") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
 }
