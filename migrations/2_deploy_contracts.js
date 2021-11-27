8be60867fa64289f8af8830748397eb8c86579f4
Riviere Fougy <rivierefougy@gmail.com>
2021-11-26 20:17:33 -0500

    2_deploy_contracts.js: changed code

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
-var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+// var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+const HTTToken = artifacts.require("HTTToken");
 
 module.exports = function (deployer) {
-  deployer.deploy(SimpleStorage, 1000000);
+  // deployer.deploy(SimpleStorage, 1000000);
+  deployer.deploy(HTTToken, 1000000);
 };
