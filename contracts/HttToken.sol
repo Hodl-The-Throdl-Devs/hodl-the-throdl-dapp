83b1f1c1b68d5f40ab23e873ae066a0185032a43
Riviere Fougy <rivierefougy@gmail.com>
2021-11-26 22:02:56 -0500

    HTTToken.sol: removed commented code

--- contracts/HttToken.sol
+++ contracts/HttToken.sol
@@ -2,14 +2,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-/*
-
-Current Capabilities of HTTToken:
-- Has supply of 1,000,000.
-- Can be transferred between accounts.
-
-*/
-
 contract HTTToken is ERC20 {
     constructor(uint256 initialSupply) ERC20("Hodl the Throdl Token", "HTT") {
         _mint(msg.sender, initialSupply * 10 ** decimals());
